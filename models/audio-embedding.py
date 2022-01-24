1d082f9bdd334bc84f00c291c37ad735bd172be7
ThomasSouflette <49478548+ThomasSouflette@users.noreply.github.com>
2022-01-24 02:27:28 +0100

    fix

--- models/audio-embedding.py
+++ models/audio-embedding.py
@@ -6,5 +6,5 @@ import soundfile as sf
 if __name__ == '__main__':
 	file_name = '/content/drive/MyDrive/AUDIOCAPT/test.wav'
 	audio, sr = sf.read(file_name)
-	embedding, timestamps = openl3.get_embedding(audio, sr)
+	embedding, timestamps = openl3.get_audio_embedding(audio, sr)
 	print(embedding)
